5f463ef3b9ec21ef9e87a4c82476bcc36c157b94
Nobi <nobi@nobidev.com>
2022-06-29 03:56:39 +0000

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,5 @@
-'use strict';
+require('./logger');
 
-import { Logger } from 'sitka';
-
-export class Example {
-	/* Private Instance Fields */
-
-	private _logger: Logger;
-
-	/* Constructor */
-
-	constructor() {
-		this._logger = Logger.getLogger({ name: this.constructor.name });
-	}
-
-	/* Public Instance Methods */
-
-	public exampleMethod(param: string): string {
-		this._logger.debug('Received: ' + param);
-		return param;
-	}
-}
+console.info('Message Info');
+console.error('Message Error');
+console.debug('Message Debug');
---
+++ src/logger.ts
@@ -0,0 +1,45 @@
+import { configure, Configuration } from 'log4js';
+
+const loggerConfig: Configuration = {
+	appenders: {
+		console: { type: 'stdout' },
+	},
+	categories: {}
+};
+
+if ('LOGSTASH_HOST' in process.env) {
+    console.log('Enabling logstash ...');
+	loggerConfig.appenders.logstash = {
+		type: '@log4js-node/logstashudp',
+		host: process.env.LOGSTASH_HOST,
+		port: +(process.env.LOGSTASH_PORT ?? 5044),
+	}
+}
+
+loggerConfig.categories.default = { appenders: Object.keys(loggerConfig.appenders), level: process.env.LOGGER_LEVEL ?? 'debug' };
+
+const logger = configure(loggerConfig).getLogger();
+
+global.console.debug = (message?: unknown, ...optionalParams: unknown[]): void => {
+	return logger.debug(message, ...optionalParams);
+}
+
+global.console.error = (message?: unknown, ...optionalParams: unknown[]): void => {
+	return logger.error(message, ...optionalParams);
+}
+
+global.console.info = (message?: unknown, ...optionalParams: unknown[]): void => {
+	return logger.info(message, ...optionalParams);
+}
+
+global.console.trace = (message?: unknown, ...optionalParams: unknown[]): void => {
+	return logger.trace(message, ...optionalParams);
+}
+
+global.console.warn = (message?: unknown, ...optionalParams: unknown[]): void => {
+	return logger.warn(message, ...optionalParams);
+}
+
+global.console.log = (message?: unknown, ...optionalParams: unknown[]): void => {
+	return logger.debug(message, ...optionalParams);
+}
